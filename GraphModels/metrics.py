25642df29b0134ee8e603b4f60ea62668dcf3264
cofofprom <nightmarelandscape@gmail.com>
2023-06-09 19:04:14 +0300

    flake8 fix

--- GraphModels/metrics.py
+++ GraphModels/metrics.py
@@ -1,24 +1,39 @@
 from scipy.stats import hmean
 
+
 def TP(x, y, z, w):
     return x
+
+
 def TN(x, y, z, w):
     return y
+
+
 def FP(x, y, z, w):
     return z
+
+
 def FN(x, y, z, w):
     return w
+
+
 def FDR(x, y, z, w):
     return z / (x + z) if x + z > 0 else 0
+
+
 def TPR(x, y, z, w):
     return x / (x + w) if x + w > 0 else 1
+
+
 def F1(x, y, z, w):
     precision = 1 - FDR(x, y, z, w)
     recall = TPR(x, y, z, w)
     return hmean([precision, recall])
 
+
 def TNR(x, y, z, w):
     return y / (y + z) if y + z > 0 else 1
+
 
 def FOR(x, y, z, w):
     return w / (y + w) if y + w > 0 else 0
--- GraphModels/solvers.py
+++ GraphModels/solvers.py
@@ -1,6 +1,6 @@
 import numpy as np
-from abc import ABC
 from scipy.stats import t, kendalltau, norm
+
 
 def pcorr_pvalues(r, n, N):
     dof = n - N
@@ -8,14 +8,17 @@ def pcorr_pvalues(r, n, N):
     pval = 2 * t.sf(np.abs(stat), dof)
     return pval
 
+
 def corr_pvalues(r, n, N=0):
     dof = n - 2
     stat = r * np.sqrt(dof / (1 - (r ** 2)))
     pval = 2 * t.sf(np.abs(stat), dof)
     return pval
 
+
 def tau_pvalues(r, n, N=0):
     return r
+
 
 def pcorrcoef(X):
     cov = np.cov(X)
@@ -23,23 +26,25 @@ def pcorrcoef(X):
     D = np.diag(1 / np.sqrt(np.diag(prec)))
     corr = -(D @ prec @ D)
     np.fill_diagonal(corr, 1)
-    
     return corr
+
 
 def tau(X):
     corr = np.zeros((X.shape[0], X.shape[0]))
     for idx1 in range(len(X)):
         for idx2 in range(idx1, len(X)):
             corr[idx1, idx2] = kendalltau(X[idx1], X[idx2]).pvalue
-            
+
     corr += corr.T + np.eye(X.shape[0])
-            
+
     return corr
-                
+
+
 def fechner_corr(x, y):
     ind = ((x - np.mean(x))*(y - np.mean(y)))
     res = (np.sum((ind >= 0)) - np.sum((ind < 0))) / len(x)
     return res
+
 
 def fechnercoef(X):
     corr = np.eye(X.shape[0])
@@ -49,48 +54,51 @@ def fechnercoef(X):
 
     return corr + corr.T - np.eye(X.shape[0])
 
+
 def fechner_pvalues(r, n, N=0):
     r = (r*n + n) / 2
     stat = (r - 0.5*n) / np.sqrt(0.5*n*0.5)
-    
+
     pvalue = 2 * norm.sf(np.abs(stat))
-    
+
     return pvalue
-    
-    
+
+
 class MHTSolver:
     def __init__(self, alpha, p_val_fun, corr_fun=np.corrcoef):
         self.alpha = alpha
         self.p_val_fun = p_val_fun
         self.corr_fun = corr_fun
-        
+
     def fit(self, X, y=None):
         self.n_obs, self.dim = X.shape
         self.n_tests = self.dim * (self.dim - 1) // 2
         self.corr_mat = self.corr_fun(X.T)
         np.fill_diagonal(self.corr_mat, 0)
-        self.p_values = np.vectorize(self.p_val_fun)(self.corr_mat, self.n_obs, self.dim)
-        
+        self.p_values = np.vectorize(self.p_val_fun)(self.corr_mat,
+                                                     self.n_obs,
+                                                     self.dim)
+
     def apply_correction(self, procedure):
         if procedure == 'SI':
             return (self.p_values < self.alpha).astype(int)
 
         if procedure == 'B':
             return (self.p_values < (self.alpha / self.n_tests)).astype(int)
-        
+
         if procedure == 'H':
             triu_idx = np.triu_indices_from(self.p_values, k=1)
-            
+
             triu_idx_list = np.array(triu_idx).T
             pvals_list = self.p_values[triu_idx]
-            
+
             perm = np.argsort(pvals_list)
-            
+
             idx_sorted = [triu_idx_list[idx] for idx in perm]
             pvals_sorted = [pvals_list[idx] for idx in perm]
-            
+
             adj = np.zeros((self.dim, self.dim))
-            
+
             for k in range(1, self.n_tests + 1):
                 curve_val = self.alpha / (self.n_tests + 1 - k)
                 if pvals_sorted[k - 1] > curve_val:
@@ -98,20 +106,20 @@ class MHTSolver:
                 adj[tuple(idx_sorted[k - 1])] = 1
                 adj[tuple(idx_sorted[k - 1])[::-1]] = 1
             return adj
-            
+
         if procedure == 'BH':
             triu_idx = np.triu_indices_from(self.p_values, k=1)
-            
+
             triu_idx_list = np.array(triu_idx).T
             pvals_list = self.p_values[triu_idx]
-            
+
             perm = np.argsort(pvals_list)
-            
+
             idx_sorted = [triu_idx_list[idx] for idx in perm]
             pvals_sorted = [pvals_list[idx] for idx in perm]
-            
+
             adj = np.ones((self.dim, self.dim)) - np.eye(self.dim)
-            
+
             for k in range(self.n_tests, 0, -1):
                 curve_val = self.alpha * k / self.n_tests
                 if pvals_sorted[k - 1] <= curve_val:
@@ -119,21 +127,21 @@ class MHTSolver:
                 adj[tuple(idx_sorted[k - 1])] = 0
                 adj[tuple(idx_sorted[k - 1])[::-1]] = 0
             return adj
-        
+
         if procedure == 'BY':
             triu_idx = np.triu_indices_from(self.p_values, k=1)
-            
+
             triu_idx_list = np.array(triu_idx).T
             pvals_list = self.p_values[triu_idx]
-            
+
             perm = np.argsort(pvals_list)
-            
+
             idx_sorted = [triu_idx_list[idx] for idx in perm]
             pvals_sorted = [pvals_list[idx] for idx in perm]
-            
+
             adj = np.ones((self.dim, self.dim)) - np.eye(self.dim)
             harm = np.log(self.n_tests) + np.euler_gamma + 1 / (2*self.n_tests)
-            
+
             for k in range(self.n_tests, 0, -1):
                 curve_val = self.alpha * k / self.n_tests / harm
                 if pvals_sorted[k - 1] <= curve_val:
@@ -141,5 +149,3 @@ class MHTSolver:
                 adj[tuple(idx_sorted[k - 1])] = 0
                 adj[tuple(idx_sorted[k - 1])[::-1]] = 0
             return adj
-        
-        
